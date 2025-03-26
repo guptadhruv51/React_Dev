c47fa44890cbd30481dd59ffa9a82e49989f78ba
Dhruv Gupta <guptadhruv51@gmail.com>
2025-03-26 16:14:42 +1100

    Basic React Done, Will learn Hooks next. Have my Telstra Interview tomorrow.

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -4,13 +4,20 @@ import Profile from './profile';
 
 function App() {
   
+  const hobby=["Running", "Coding"]
+  const handleHobbyClick=(hobby)=>
+  {
+    alert(`You clicked on:${ hobby}`);
+  };
+
 
   return (
-    <>
+    <> 
       <h1>Hello</h1>
       {/* <Profile name="Dhruv" age={26} isMemeber="true"/> //String */}
-      <Profile name="Dhruv" age={26} isMember={true}/> 
-      <Profile name="Dhruv" age={26} isMember={true}/> 
+      <Profile name="Dhruv" age={26} isMember={true} hobbies={hobby}
+      onHobbyClick={handleHobbyClick}/> 
+      <Profile name="Dhruv" age={26} isMember={true} hobbies={hobby} onHobbyClick={handleHobbyClick}/> 
     </>
   );
 }
--- Props/src/profile.jsx
+++ Props/src/profile.jsx
@@ -8,6 +8,18 @@ function Profile(prop)
     <h4>Name:{prop.name}</h4>
     <h4>Age:{prop.age}</h4>
     <h4>Status: {prop.isMember?"Active Member":"Guest"}</h4>
+    <h4>Hobbies</h4>
+    <ul>
+      {
+        prop.hobbies.map((hobby,index)=>
+        {
+           return <li key={index} onClick={()=>prop.onHobbyClick(hobby)}>{hobby} </li>;
+        })
+      }
+    </ul>
+
+
+
     </>
   )
 }
