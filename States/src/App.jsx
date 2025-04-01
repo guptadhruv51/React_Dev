3ff04e6f2be61ec8637ab72d496a8ba850a3dd0b
Dhruv Gupta <guptadhruv51@gmail.com>
2025-04-01 16:49:16 +1100

    Understood States

---
+++ States/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [count,setCount]=useState(0);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Hello</h1>
+        <h2>Counter value: {count}</h2>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
