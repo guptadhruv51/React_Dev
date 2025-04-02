d0b77f88dc8f164e79b95de61bb8929568250a05
Dhruv Gupta <guptadhruv51@gmail.com>
2025-04-02 15:14:34 +1100

    Understood async nature of States

--- States/src/App.jsx
+++ States/src/App.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 function App() {
   const [count,setCount]=useState(0);
 
+
+  
+  const incrementTwice=()=>
+  {
+    setCount(prevCount=>prevCount+1);
+    setCount(prevCount=>prevCount+1);
+  }
+  // States updates are async 
+  // Updater function for multiple states 
   return (
     <>
       <div className="container">
@@ -10,6 +19,7 @@ function App() {
         <h2>Counter value: {count}</h2>
         <button onClick={()=>setCount(count+1)}>Increment</button>
         <button onClick={()=>setCount(count-1)}>Decrement</button>
+        <button onClick={incrementTwice}>+2</button>
       </div>
     </>
   );
